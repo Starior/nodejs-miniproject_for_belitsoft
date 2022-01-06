c79567cd1cf5e58c7bbdd553f75a2110f157d370
Starior <zzzartem@gmail.com>
2022-01-06 18:03:12 +0300

    New logger

--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -8,14 +8,63 @@ const writeLog = (req, res, next) => {
     body: req.body,
     date: new Date().toLocaleDateString(),
     time: new Date().toLocaleTimeString('en-US', { hour12: false })
-      // time: Date.now()
   };
   var json = JSON.stringify(obj, null, 2);
   // var json = JSON.stringify(obj);
-  fs.appendFile('./logs.json', json + "\n", () => {
-    console.log('logs.json was updated');
-  })
+
+
+
+  if (checkFileExistsSync('./logs.json')) {
+    // console.log('file exists');
+    let rawdata = fs.readFileSync('./logs.json');
+    if (validateJSON(rawdata)) {
+      // console.log('file json');
+      let rawdata = fs.readFileSync('./logs.json');
+      const myJSON = JSON.parse(rawdata);
+      myJSONString = JSON.stringify(myJSON, null, 2);
+      if (myJSONString.slice(myJSONString.length - 1) === "]") {
+        // 3 & subsequent logs
+        myJSONString = myJSONString.slice(1, myJSONString.length - 1)
+        fs.writeFileSync('./logs.json', "[" + myJSONString + ",\n" + json + "\n]"); // 
+      } else {
+        // 1 & 2 logs
+        fs.writeFileSync('./logs.json', "[" + rawdata + ",\n" + json + "]");
+      }
+    } else {
+      // console.log('file emty (not json)');
+      fs.writeFileSync('./logs.json', json);
+    }
+  } else {
+    // console.log('file does not exists');
+    fs.writeFileSync('./logs.json', json);
+  }
+
+  //old
+  // fs.appendFile('./logs.json', json + "\n", () => {
+  //   console.log('logs.json was updated');
+  // })
+
   next();
 }
 
+function checkFileExistsSync(filepath) {
+  let flag = true;
+  try {
+    fs.accessSync(filepath, fs.constants.F_OK);
+  } catch (e) {
+    flag = false;
+  }
+  return flag;
+}
+
+function validateJSON(body) {
+  try {
+    var data = JSON.parse(body);
+    // if came to here, then valid
+    return data;
+  } catch (e) {
+    // failed to parse
+    return false;
+  }
+}
 module.exports = { writeLog }
